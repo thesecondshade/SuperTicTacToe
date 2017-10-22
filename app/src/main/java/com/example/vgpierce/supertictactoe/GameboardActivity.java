bd22c96d2ef9447bd0eb078142546ed926a15740
Victoria Pierce <vgpierce@spscommerce.com>
2017-10-21 21:07:59 -0500

    fixes problemos

---
+++ app/src/main/java/com/example/vgpierce/supertictactoe/GameboardActivity.java
@@ -0,0 +1,26 @@
+package com.example.vgpierce.supertictactoe;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+
+/**
+ * Created by vgpierce on 10/21/17.
+ */
+
+
+
+public class GameboardActivity extends Activity {
+    draw drawView;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        drawView = new draw(this);
+        drawView.setBackgroundColor(Color.WHITE);
+        setContentView(drawView);
+
+        }
+    }
+
