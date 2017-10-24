7a344aa2057b6696d63587eb71e12b0210b7f8ed
Alex McKee <alexander.mckeever@gmail.com>
2017-10-23 21:51:24 -0500

    Makes draw TicTacToe board function and adds tap detection on all gameboards. Lots of dead code that needs to be shoveled out.

--- app/src/main/java/com/example/vgpierce/supertictactoe/TicTacToe.java
+++ app/src/main/java/com/example/vgpierce/supertictactoe/TicTacToe.java
@@ -1,5 +1,6 @@
 package com.example.vgpierce.supertictactoe;
 
+import android.graphics.Point;
 import android.util.Log;
 
 /**
@@ -9,12 +10,17 @@ import android.util.Log;
 
 public class TicTacToe {
     Integer[][] gameSlots = new Integer[3][3];
+    float posX,posY,width,height;
     public TicTacToe(){
         for(int x = 0; x<gameSlots.length;x++){
             for(int y = 0;y< gameSlots[0].length;y++){
                 gameSlots[x][y] = 0;
             }
         }
+        this.posX = posX;
+        this.posY = posY;
+        this.width = width;
+        this.height = height;
     }
     public boolean setValue(int x, int y, int value){
         if(gameSlots[x][y] == 0){
@@ -22,6 +28,26 @@ public class TicTacToe {
             return true;
         }
         return false;
+    }
+    public Point getGridValue(float pX, float pY){
+        float incValueX = width * (1f/3f);
+        float incValueY = height * (1f/3f);
+        Point p = new Point();
+        for(int x = 0;x<gameSlots.length;x++) {
+            for (int y = 0; y < gameSlots[0].length; y++) {
+                //Checks if tap position is within bounds of each square
+                if (pX > posX + (x * incValueX) && pX < posX + ((x + 1) * incValueX) && pY > posY + (y * incValueY) && pY < posY + ((y + 1) * incValueY)) {
+                        //Possibly redundant
+                    p.x = x;
+                    p.y = y;
+                }
+            }
+        }
+        return p;
+    }
+    public boolean makeMove(float pX, float pY, int value){
+        Point point = getGridValue(pX,pY);
+        return setValue(point.x,point.y,value);
     }
     public boolean checkVictory(){
         if(gameSlots[1][1] !=0){
@@ -63,6 +89,24 @@ public class TicTacToe {
     public Integer[][] getGameBoard(){
             Log.d("Test",toString());
         return gameSlots;
+    }
+    public float getPosX(){
+        return posX;
+    }
+    public float getPosY(){
+        return posY;
+    }
+    public float getWidth(){
+        return width;
+    }
+    public float getHeight(){
+        return height;
+    }
+    public void setDimensions(float x,float y,float w, float h){
+        posX = x;
+        posY = y;
+        width = w;
+        height = h;
     }
     public String toString(){
         String a = "";
--- app/src/main/java/com/example/vgpierce/supertictactoe/draw.java
+++ app/src/main/java/com/example/vgpierce/supertictactoe/draw.java
@@ -3,6 +3,8 @@ package com.example.vgpierce.supertictactoe;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.MotionEvent;
 import android.view.View;
 import android.util.*;
 import android.content.Context;
@@ -16,9 +18,18 @@ public class draw extends View {
     private void init() {
         paint.setColor(Color.BLACK);
     }
-
+    float xPos = 0;
+    float yPos = 0;
+    //1 for X's
+    //-1 for O's
+    int playerTurn = 1;
+    TicTacToe masterTicTacToeBoard = new TicTacToe();
+    TicTacToe[] gameBoards = new TicTacToe[9];
     public draw(Context context) {
         super(context);
+        for(int i = 0;i<9;i++){
+            gameBoards[i] = new TicTacToe();
+        }
         init();
     }
 
@@ -35,26 +46,29 @@ public class draw extends View {
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int x = canvas.getWidth();
-        int y = canvas.getHeight();
-        int[][]  corners  =  {{0, 0},  {x/3, y/3},
-                {x/3, 0},  {(2*x)/3, y/3},
-                {(2*x)/3,0}, {x, y / 3},
-                {0, y/3}, {x/3,(2*y)/3},
-                {x/3,y/3}, {(2*x)/3,(2*y)/3},
-                {(2*x)/3,y/3}, {x, (2*y)/3},
-                {0,(2*y)/3}, {x/3,y},
-                {x/3,(2*y)/3}, {(2*x)/3, y},
-                {(2*x)/3,(2*y)/3},{x,y},
-                {0,  0}, {x, y}
-
+        int cW = canvas.getWidth();
+        int cH = canvas.getHeight();
+//        int[][]  corners  =  {{0, 0},  {x/3, y/3},
+//                {x/3, 0},  {(2*x)/3, y/3},
+//                {(2*x)/3,0}, {x, y / 3},
+//                {0, y/3}, {x/3,(2*y)/3},
+//                {x/3,y/3}, {(2*x)/3,(2*y)/3},
+//                {(2*x)/3,y/3}, {x, (2*y)/3},
+//                {0,(2*y)/3}, {x/3,y},
+//                {x/3,(2*y)/3}, {(2*x)/3, y},
+//                {(2*x)/3,(2*y)/3},{x,y},
+//        };
+        int[][]  corners  =  {{0, 0},  {cW/3, 0}, {2*cW/3, 0},
+                {0, cH/3}, {cW/3,cH/3}, {2*cW/3, cH/3},
+                {0, 2*cH/3}, {cW/3,2*cH/3}, {2*cW/3, 2*cH/3}
         };
+        masterTicTacToeBoard.setDimensions(0,0,cW,cH);
+        drawTicTacToeBoard(canvas, masterTicTacToeBoard);
 
-        for(int i =0; i< corners.length;i+=2) {
-            game(canvas,corners[i][0],corners[i][1],corners[i+1][0],corners[i+1][1]);
-
+        for(int i = 0;i<gameBoards.length;i++){
+            gameBoards[i].setDimensions(corners[i][0],corners[i][1],cW/3,cH/3);
+            drawTicTacToeBoard(canvas,gameBoards[i]);
         }
-
 
     }
     public void game(Canvas canvas, int x1, int y1, int x2, int y2) {
@@ -78,6 +92,57 @@ public class draw extends View {
             canvas.drawLine(x1, y1+ytwo, x2, y1+ytwo, paint);
         }
 
+    }
+    public void drawTicTacToeBoard(Canvas canvas,TicTacToe ttc){
+        paint.setStrokeWidth(3);
+        float posX = ttc.getPosX();
+        float posY = ttc.getPosY();
+        float width = ttc.getWidth();
+        float height = ttc.getHeight();
+        if(ttc.width == canvas.getWidth() && ttc.height == canvas.getHeight())
+            paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
+        float third = (1f/3f);
+        canvas.drawRect(posX,posY,width,height,paint);
+        canvas.drawLine(posX,posY+(third)*height,posX+width,posY+(third)*height, paint);
+        canvas.drawLine(posX,posY+(third*2)*height,posX+width,posY+(third*2)*height, paint);
+        canvas.drawLine(posX+(third)*width,posY,posX+(third)*width,posY+height, paint);
+        canvas.drawLine(posX+(third*2)*width,posY,posX+(third*2)*width,posY+height, paint);
+        Integer[][] playsOnBoard = ttc.getGameBoard();
+        float incValueX = width * third;
+        float incValueY = height * third;
+        for(int x = 0; x<playsOnBoard.length;x++){
+            for(int y = 0;y<playsOnBoard[0].length;y++){
+                if(playsOnBoard[x][y] == 1){
+                    canvas.drawLine(posX +(x*incValueX),posY+(y*incValueY),posX +((x+1)*incValueX),posY+((y+1)*incValueY),paint);
+                    canvas.drawLine(posX +(x*incValueX),posY+((y+1)*incValueY),posX +((x+1)*incValueX),posY+(y*incValueY),paint);
+                }
+                else if(playsOnBoard[x][y] == -1){
+                    canvas.drawOval(posX +(x*incValueX),posY+(y*incValueY),posX +((x+1)*incValueX),posY+((y+1)*incValueY),paint);
+                    canvas.drawOval(posX +(x*incValueX),posY+((y+1)*incValueY),posX +((x+1)*incValueX),posY+(y*incValueY),paint);
+                }
+            }
+        }
+
+    }
+    @Override
+    public boolean onTouchEvent (MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN :
+                xPos = event.getX();
+                yPos = event.getY();
+                Point gridPoint = masterTicTacToeBoard.getGridValue(xPos,yPos);
+                Log.d("Tic Tac Toe",gridPoint.toString());
+                if(gameBoards[(gridPoint.x)+(gridPoint.y*3)].makeMove(xPos,yPos,playerTurn)){
+                    playerTurn *=-1;
+                }
+
+                Log.d("Touch Event","Touched at " + xPos + " " + yPos);
+                invalidate();
+                break;
+        }
+
+        return true;
 
     }
 
